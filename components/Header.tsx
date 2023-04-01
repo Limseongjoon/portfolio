ef2b3c960cbb15d9e9b04399baf4a66a7706849b
Limseongjoon <sungjun1703@snu.ac.kr>
2023-04-02 01:07:07 +0900

    [feat]contact_finish

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,23 +3,23 @@ import Link from 'next/link';
 
 const Page = () => {
   return (
-    <div className="fixed z-5 w-screen px-10 lg:px-12 bg-white">
+    <div className="fixed z-5 w-screen px-6 lg:px-12">
       <div className="h-5 md:h-6 lg:h-8"></div>
       <div className="flex items-start">
         <Link href="/">
           <CustomImage src="/images/logo_joon.png" className="h-4 md:h-5 lg:h-6 w-auto" />
         </Link>
         <div className="grow"></div>
-        <div className="flex items-end gap-8 lg:gap-12 lg:-my-1">
+        <div className="flex items-end gap-6 lg:gap-12 lg:-my-1">
           <Link
             href="/#"
-            className="font-hrot text-xs lg:text-sm"
+            className="font-hrot text-2xs lg:text-sm"
           >
             ABOUT
           </Link>
           <Link
             href="/contact"
-            className="font-hrot text-xs lg:text-sm"
+            className="font-hrot text-2xs lg:text-sm"
           >
             CONTACT
           </Link>
--- pages/contact.tsx
+++ pages/contact.tsx
@@ -2,35 +2,37 @@ import Header from '../components/Header';
 
 const Page = () => {
   return (
-    <div className="flex flex-col items-center bg-white">
+    <div>
       <Header />
-      <div className="h-32 md:h-36 lg:h-48"></div>
-        <div className="h-12 md:h-16 lg:h-20"></div>
-        <div className="font-hrot text-center text-lg md:text-xl lg:text-2xl">
-          CONTACT
+      <div className="flex flex-col h-screen items-center">
+        <div className="h-48 md:h-56 lg:h-64" />
+          <div className="font-hrot text-center text-lg md:text-xl lg:text-2xl">
+            CONTACT
+          </div>
+          <div className="h-4" />
+          <div className="hidden sm:flex text-center font-light md:text-lg">
+          Please feel free to contact me with any questions you may have.<br></br>
+          I will get back to you as soon as possible.
+          </div>
+          <div className="sm:hidden text-center font-light md:text-lg">
+          Please feel free to contact me<br></br> with any questions you may have.<br></br>
+          I will get back to you as soon as possible.
+          </div>
+        <div className="h-10" />
+        <div className="font-hrot text-center text-sm md:text-base">
+          <a
+            href="mailto:sungjun170@gmail.com"
+            className="px-6 py-1 hover:bg-black hover:text-white focus:bg-black focus:text-white"
+          >
+            sungjun170@gmail.com
+          </a>
         </div>
-        <div className="h-4"></div>
-        <div className="hidden sm:flex text-center font-light md:text-lg">
-        Please feel free to contact me with any questions you may have.<br></br>
-        I will get back to you as soon as possible.
-        </div>
-        <div className="sm:hidden text-center font-light md:text-lg">
-        Please feel free to contact me<br></br> with any questions you may have.<br></br>
-        I will get back to you as soon as possible.
-        </div>
-      <div className="h-10" />
-      <div className="font-hrot text-center text-sm md:text-base">
-        <a
-          href="mailto:sungjun170@gmail.com"
-          className="px-6 py-1 hover:bg-black hover:text-white focus:bg-black focus:text-white"
-        >
-          sungjun170@gmail.com
-        </a>
-      </div>
-      <div className="h-96 lg:h-72"></div>
-      <div className="w-full h-[120px] lg:h-[240px] text-center">
-        <div className="text-xs md:text-sm lg:text-base">
-          Copyright © IM SEONGJOON. All rights Reserved.
+        <div className="grow" />
+        <div className="h-36" />
+        <div className="w-full h-24 text-center">
+          <div className="text-xs md:text-sm lg:text-base">
+            Copyright © IM SEONGJOON. All rights Reserved.
+          </div>
         </div>
       </div>
     </div>

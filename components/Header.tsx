e021cc1a07a625bc4760b085966428ec67c8eb42
Limseongjoon <sungjun1703@snu.ac.kr>
2023-04-14 14:17:56 +0900

    f

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,7 +6,7 @@ const Header = () => {
     <div className="fixed z-50 w-screen bg-transparent font-hrot px-3 sm:px-4 lg:px-5s">
       <div className="h-1" />
       <div className="flex flex-row justify-between">
-        <div className="flex gap-4 sm:gap-12 lg:gap-16">
+        <div className="flex gap-2 sm:gap-12 lg:gap-16">
           <Link
             href="/"
             className="flex text-xs lg:text-sm text-black hover:-rotate-6 hover:font-bold active:-rotate-12 active:font-bold"
@@ -20,7 +20,7 @@ const Header = () => {
             ← words
           </Link>
         </div>
-        <div className="flex gap-4 sm:gap-12 lg:gap-16">
+        <div className="flex gap-2 sm:gap-12 lg:gap-16">
           <Link
             href="/about"
             className="flex flex-row text-xs lg:text-sm text-black hover:rotate-6 hover:font-bold active:rotate-12 active:font-bold"

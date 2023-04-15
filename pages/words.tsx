04c0762b255b86493a63b50c995933a5642e89ac
Limseongjoon <sungjun1703@snu.ac.kr>
2023-04-15 20:14:47 +0900

    l

--- pages/words.tsx
+++ pages/words.tsx
@@ -15,8 +15,81 @@ const Page = () => {
             <Accordion.Panel>
               <Accordion.Title>
                 <div className="font-hrot font-semibold text-lg text-black">
-                  In-der-Welt-sein
+                  Why I developed?
                 </div>
+              </Accordion.Title>
+              <Accordion.Content>
+                <div className="my-2">
+                  Started for my portfolio. 프로젝트에만 몰입하고자.
+                  포트폴리오는 결과물을 담는 지면일 뿐인데, 결과물의 모습에
+                  집중하는 나를 발견했다. 도구인 매체가 목적보다 우선하게 되는
+                  순간 현재에 몰입하지 못하는 나를 발견했다. 자료 업로드 시의
+                  규격부터 생각하게 되고, 제한.
+                </div>
+              </Accordion.Content>
+            </Accordion.Panel>
+            <Accordion.Panel>
+              <Accordion.Title>
+                <p className="font-hrot font-semibold text-lg text-black">
+                  What is Design?
+                </p>
+              </Accordion.Title>
+              <Accordion.Content>
+                <div className="mt-2">
+                  Essentially ahead of itself, it has projected itself upon its
+                  potentiality-for-Being before going on to any mere
+                  consideration of itself. Being-in-the-world has always
+                  expressed itself, and as Being alongside entities encountered
+                  within-the-world, it constantly expresses itself in addressing
+                  itself to the very object of its concern and discussing it.
+                </div>
+                <div className="h-2 sm:h-4"></div>
+                <div className="text-sm mb-2 font-hrot">⎯ Martin Heidegger</div>
+              </Accordion.Content>
+            </Accordion.Panel>
+            <Accordion.Panel>
+              <Accordion.Title>
+                <p className="font-hrot font-semibold text-lg text-black">
+                  Methodology
+                </p>
+              </Accordion.Title>
+              <Accordion.Content>
+                <div className="mt-2">
+                  Essentially ahead of itself, it has projected itself upon its
+                  potentiality-for-Being before going on to any mere
+                  consideration of itself. Being-in-the-world has always
+                  expressed itself, and as Being alongside entities encountered
+                  within-the-world, it constantly expresses itself in addressing
+                  itself to the very object of its concern and discussing it.
+                </div>
+                <div className="h-2 sm:h-4"></div>
+                <div className="text-sm mb-2 font-hrot">⎯ Martin Heidegger</div>
+              </Accordion.Content>
+            </Accordion.Panel>
+            <Accordion.Panel>
+              <Accordion.Title>
+                <p className="font-hrot font-semibold text-lg text-black">
+                  Operator vs. Director
+                </p>
+              </Accordion.Title>
+              <Accordion.Content>
+                <div className="mt-2">
+                  Essentially ahead of itself, it has projected itself upon its
+                  potentiality-for-Being before going on to any mere
+                  consideration of itself. Being-in-the-world has always
+                  expressed itself, and as Being alongside entities encountered
+                  within-the-world, it constantly expresses itself in addressing
+                  itself to the very object of its concern and discussing it.
+                </div>
+                <div className="h-2 sm:h-4"></div>
+                <div className="text-sm mb-2 font-hrot">⎯ Martin Heidegger</div>
+              </Accordion.Content>
+            </Accordion.Panel>
+            <Accordion.Panel>
+              <Accordion.Title>
+                <p className="font-hrot font-semibold text-lg text-black">
+                  My journey
+                </p>
               </Accordion.Title>
               <Accordion.Content>
                 <div className="mt-2">
@@ -38,15 +111,35 @@ const Page = () => {
                 </p>
               </Accordion.Title>
               <Accordion.Content>
-                <p className="my-3">
-                  Lorem Ipsum is simply dummy text of the printing and
-                  typesetting industry. Lorem Ipsum has been the industry
-                  standard dummy text ever since the 1500s, when an unknown
-                  printer took a galley of type and scrambled it to make a type
-                  specimen book. It has survived not only five centuries, but
-                  also the leap into electronic typesetting, remaining
-                  essentially unchanged.
+                <div className="mt-2">
+                  Essentially ahead of itself, it has projected itself upon its
+                  potentiality-for-Being before going on to any mere
+                  consideration of itself. Being-in-the-world has always
+                  expressed itself, and as Being alongside entities encountered
+                  within-the-world, it constantly expresses itself in addressing
+                  itself to the very object of its concern and discussing it.
+                </div>
+                <div className="h-2 sm:h-4"></div>
+                <div className="text-sm mb-2 font-hrot">⎯ Martin Heidegger</div>
+              </Accordion.Content>
+            </Accordion.Panel>
+            <Accordion.Panel>
+              <Accordion.Title>
+                <p className="font-hrot font-semibold text-lg text-black">
+                  Thesedays
                 </p>
+              </Accordion.Title>
+              <Accordion.Content>
+                <div className="mt-2">
+                  Essentially ahead of itself, it has projected itself upon its
+                  potentiality-for-Being before going on to any mere
+                  consideration of itself. Being-in-the-world has always
+                  expressed itself, and as Being alongside entities encountered
+                  within-the-world, it constantly expresses itself in addressing
+                  itself to the very object of its concern and discussing it.
+                </div>
+                <div className="h-2 sm:h-4"></div>
+                <div className="text-sm mb-2 font-hrot">⎯ Martin Heidegger</div>
               </Accordion.Content>
             </Accordion.Panel>
           </Accordion>

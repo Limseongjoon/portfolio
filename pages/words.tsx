d421c78a006c5570df14ea378b62f21848c76178
Limseongjoon <sungjun1703@snu.ac.kr>
2023-04-15 21:50:09 +0900

    ㅇ

--- pages/words.tsx
+++ pages/words.tsx
@@ -19,13 +19,11 @@ const Page = () => {
                 </div>
               </Accordion.Title>
               <Accordion.Content>
-                <div className="h-2" />
-                <div className="md:w-4/5 break-keep">
+                <div className="md:w-4/5 break-keep ml-6">
                   결과물을 담는 도구일뿐인 매체를 절대적으로 배제하여 현재의
                   가치에 몰입하고, 프로젝트 작업 매체의 규격과 포맷에 상관없이
                   최적의 포트폴리오 구성 환경을 조직하기 위하여.
                 </div>
-                <div className="h-2" />
               </Accordion.Content>
             </Accordion.Panel>
             <Accordion.Panel>
@@ -35,8 +33,7 @@ const Page = () => {
                 </p>
               </Accordion.Title>
               <Accordion.Content>
-                <div className="h-2" />
-                <div className="md:w-4/5 break-keep">
+                <div className="md:w-4/5 break-keep ml-6">
                   <div>
                     디자인은 필요로 시작하여 논리와 근거를 바탕으로 감각의
                     총체로 맞이하도록 만드는 방법론이기에, 명확한 필요, 논리,
@@ -48,13 +45,7 @@ const Page = () => {
                     벗어난다. 나아가 넛지함의 권력을 가질 수 있으므로 조심스럽게
                     다뤄야 한다.
                   </div>
-                  <div className="h-3" />
-                  <div>
-                    나아가 넛지함의 권력을 가질 수 있으므로 조심스럽게 다뤄야
-                    한다.
-                  </div>
                 </div>
-                <div className="h-2" />
               </Accordion.Content>
             </Accordion.Panel>
             <Accordion.Panel>
@@ -64,73 +55,46 @@ const Page = () => {
                 </p>
               </Accordion.Title>
               <Accordion.Content>
-                <div className="h-2" />
-                <div className="md:w-4/5 break-keep">
+                <div className="md:w-4/5 break-keep ml-6">
                   <div>
-                    디자인은 필요로 시작하여 논리와 근거를 바탕으로 감각의
-                    총체로 맞이하도록 만드는 방법론이다.
+                    1. <span className="font-semibold">레거시</span>와{" "}
+                    <span className="font-semibold">보편</span>을 구성하는
+                    요소들을 추출하여 조형으로 읽어내기.
                   </div>
                   <div className="h-2" />
                   <div>
-                    필요로써 예술과 구별되고 논리와 근거로써 비로소 모방에서
-                    벗어난다. 나아가 넛지함의 권력을 가지기 쉬우므로 절대 함부로
-                    다뤄서는 안된다. 명확한 필요, 논리, 근거가 있다면 그럴 수
-                    밖에 없는 결과가 도출된다.
+                    2. 위 조형 요소들을 이용하여 과도하지 않으나 통용될 수 있는,
+                    새롭고 강렬한 충격을 주는{" "}
+                    <span className="font-semibold">적절한 지형도</span>{" "}
+                    그려내기.
+                  </div>
+                  <div className="h-2" />
+                  <div>
+                    3. 프로젝트 성격에 부합하는{" "}
+                    <span className="font-semibold">이성-감각</span> 비율 조정.
                   </div>
                 </div>
-                <div className="h-2" />
-              </Accordion.Content>
-            </Accordion.Panel>
-
-            <Accordion.Panel>
-              <Accordion.Title>
-                <p className="font-hrot font-semibold text-lg text-black">
-                  Journey
-                </p>
-              </Accordion.Title>
-              <Accordion.Content>
-                <div className="mt-2">
-                  Essentially ahead of itself, it has projected itself upon its
-                  potentiality-for-Being before going on to any mere
-                  consideration of itself. Being-in-the-world has always
-                  expressed itself, and as Being alongside entities encountered
-                  within-the-world, it constantly expresses itself in addressing
-                  itself to the very object of its concern and discussing it.
-                </div>
               </Accordion.Content>
             </Accordion.Panel>
             <Accordion.Panel>
               <Accordion.Title>
                 <p className="font-hrot font-semibold text-lg text-black">
-                  Norms
+                  Phrase hit me hard
                 </p>
               </Accordion.Title>
               <Accordion.Content>
-                <div className="mt-2">
+                <div className="md:w-4/5 break-keep ml-6">
                   Essentially ahead of itself, it has projected itself upon its
                   potentiality-for-Being before going on to any mere
-                  consideration of itself. Being-in-the-world has always
-                  expressed itself, and as Being alongside entities encountered
-                  within-the-world, it constantly expresses itself in addressing
-                  itself to the very object of its concern and discussing it.
+                  consideration of itself.{" "}
+                  <span className="font-semibold">Being-in-the-world</span> has
+                  always expressed itself, and as Being alongside entities
+                  encountered within-the-world, it constantly expresses itself
+                  in addressing itself to the very object of its concern and
+                  discussing it.
                 </div>
-              </Accordion.Content>
-            </Accordion.Panel>
-            <Accordion.Panel>
-              <Accordion.Title>
-                <p className="font-hrot font-semibold text-lg text-black">
-                  Thesedays
-                </p>
-              </Accordion.Title>
-              <Accordion.Content>
-                <div className="mt-2">
-                  Essentially ahead of itself, it has projected itself upon its
-                  potentiality-for-Being before going on to any mere
-                  consideration of itself. Being-in-the-world has always
-                  expressed itself, and as Being alongside entities encountered
-                  within-the-world, it constantly expresses itself in addressing
-                  itself to the very object of its concern and discussing it.
-                </div>
+                <div className="h-4" />
+                <div className="font-hrot text-sm ml-6">― Martin Heidegger</div>
               </Accordion.Content>
             </Accordion.Panel>
           </Accordion>
